8b16e9c644bb8bb1bc5e734025cfc4e12c8ff326
PP_Pro <chinese1236@qq.com>
2020-09-10 18:02:17 +0800

    update

--- mainProject/assets/TestScript.ts
+++ mainProject/assets/TestScript.ts
@@ -6,12 +6,6 @@ export default class NewClass extends cc.Component {
     @property(cc.Node)
     subContextNode: cc.Node = null;
 
-    @property(cc.SubContextView)
-    subContextComp: cc.SubContextView = null;
-
-    _tweening = false;
-    _viewVisible = true;
-
     onLoad () {
         let env = window.wx || window.tt || window.swan;
         if (env) {
